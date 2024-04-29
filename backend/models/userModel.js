1aae424bc52e683601a5a4569e75c68796f03201
Abhijith <abhijithm050@gmail.com>
2024-04-29 11:39:07 +0530

    added fronted ui

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const { timeStamp } = require('console')
+const mongoose = require('mongoose')
+const { type } = require('os')
+
+const userSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, 'Please add a name']
+    },
+    email : {
+        type : String,
+        required : [true, 'Please add an email'],
+        unique : true
+    },
+    password : {
+        type : String,
+        required : [true, 'Please add a password']
+    },
+    phone : {
+        type : Number,
+        required : [true, 'Please add a phone number']
+    },
+},
+{
+    timeStamp : true
+})
+
+module.exports = mongoose.model("User",userSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+const express = require('express')
+const colors = require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoute'))
+app.use(errorHandler)
+app.listen(port, () => console.log(`server started in http://localhost:${port}`.yellow))
